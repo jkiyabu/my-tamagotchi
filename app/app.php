59977ebfa4faa6d6bdf89718f85bf019e444e0b2
Jayeson Kiyabu and Philip Putnam <jayeson@>
2017-02-08 11:54:08 -0800

    add initial files and display title on root page

---
+++ app/app.php
@@ -0,0 +1,26 @@
+<?php
+    date_default_timezone_set("America/Los_Angeles");
+    require_once __DIR__.'/../vendor/autoload.php';
+    require_once __DIR__.'/../src/Tamagotchi.php';
+
+    session_start();
+
+    if (empty($_SESSION['tamagotchis'])) {
+        $_SESSION['tamagotchis'] = array();
+    };
+
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use ($app) {
+
+        return $app['twig']->render('root.html.twig');
+
+    });
+
+
+    return $app;
+?>
---
+++ src/Tamagotchi.php
@@ -0,0 +1,40 @@
+<?php
+    class Tamagotchi
+    {
+        private $name;
+        private $sleep;
+        private $happiness;
+        private $hunger;
+
+        function __construct($name) {
+            $this->name = $name;
+            $this->sleep = 40;
+            $this->happiness = 25;
+            $this->hunger = 25;
+        }
+
+        function
+
+        function setSleep($new_sleep) {
+            $this->sleep = $new_sleep;
+        }
+
+        function getSleep() {
+            return $this->sleep;
+        }
+
+        function setHappiness($new_happiness) {
+            $this->happiness = $new_happiness;
+        }
+
+        function getHappiness() {
+            return $this->happiness;
+        }
+
+        function set
+
+
+
+    }
+
+?>
